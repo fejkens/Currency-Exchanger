0a1727b10baeeee4472810094e9e848ced0779f0
fejkens <lol85251@gmail.com>
2021-01-26 15:29:46 +0000

    Created a valid sample request

--- exchange.py
+++ exchange.py
@@ -1,10 +1,15 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import requests
 
 dotenv_path = join(dirname(__file__), '.gitignore/.env')
 load_dotenv(dotenv_path)
 
-key = os.getenv('CURRENCY_API_KEY')
+BASE_URL = 'https://www.amdoren.com/api/currency.php'
+CURRENCY_API_KEY = os.getenv('CURRENCY_API_KEY')
 
-print(key)
+req = requests.get(f'{BASE_URL}?api_key={CURRENCY_API_KEY}&from=USD&to=GBP&amount=1')
+print(req)
+
+
