97459ef1f66eb9e0542bd75f53ec508bd5f8a2aa
fejkens <lol85251@gmail.com>
2021-01-26 15:40:37 +0000

    Added arguments for exchange

--- exchange.py
+++ exchange.py
@@ -2,6 +2,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import requests
+import sys
 
 dotenv_path = join(dirname(__file__), '.gitignore/.env')
 load_dotenv(dotenv_path)
@@ -9,7 +10,13 @@ load_dotenv(dotenv_path)
 BASE_URL = 'https://www.amdoren.com/api/currency.php'
 CURRENCY_API_KEY = os.getenv('CURRENCY_API_KEY')
 
-req = requests.get(f'{BASE_URL}?api_key={CURRENCY_API_KEY}&from=USD&to=GBP&amount=1')
-print(req)
+def main():
 
+    amount = sys.argv[1]
+    base = sys.argv[2]
+    to = sys.argv[3]
 
+    req = requests.get(f'{BASE_URL}?api_key={CURRENCY_API_KEY}&from={base}&to={to}&amount={amount}')
+
+if __name__ == '__main__':
+    main()
