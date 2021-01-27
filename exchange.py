83ecf829d5cf421615083573aed8125869f302f7
fejkens <lol85251@gmail.com>
2021-01-27 16:23:12 +0000

    Fixed the calculation accuracy

--- exchange.py
+++ exchange.py
@@ -12,13 +12,22 @@ CURRENCY_API_KEY = os.getenv('CURRENCY_API_KEY')
 
 def main():
 
-    amount = sys.argv[1]
+    amount = int(sys.argv[1])
     base = sys.argv[2]
     to = sys.argv[3]
     symbols = base + ',' + to
 
     req = requests.get(f'{BASE_URL}?access_key={CURRENCY_API_KEY}&symbols={symbols}&amount={amount}')
-    print(req.json())
+    req = req.json()
+
+    exchange_rate = req["rates"][to] / req["rates"][base]
+
+    exchange_result = format(amount * exchange_rate, '.2f')
+
+    exchange_rate = format(exchange_rate, '.5f')
+
+    message = f'{amount} {base} = {exchange_result} {to} ({exchange_rate} EX)'
+    print(message)
 
 if __name__ == '__main__':
     main()
