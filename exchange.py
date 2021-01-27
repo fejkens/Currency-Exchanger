bbf4bcc8ff848f1f07be5551069885ef0e3e0b8f
fejkens <lol85251@gmail.com>
2021-01-27 19:25:56 +0000

    Separated the API call into a new function and tidied up the code

--- exchange.py
+++ exchange.py
@@ -12,30 +12,37 @@ load_dotenv(dotenv_path)
 BASE_URL = os.getenv('BASE_URL')
 CURRENCY_API_KEY = os.getenv('CURRENCY_API_KEY')
 
+
 def main():
 
     # Grab the sys arguments passed in when program is started
-    amount = int(sys.argv[1])
+    amount = float(sys.argv[1])
     base = sys.argv[2]
     to = sys.argv[3]
+    
+    exchange_rate = fetch_exchange_rate(amount, base, to)
+
+    # Calculate the resulting amount of the exchange
+    exchange_result = amount * exchange_rate
+
+    # Print out the result of the exchange
+    message = f'{amount} {base} = {round(exchange_result, 2)} {to} ({round(exchange_rate, 5)} EX)'
+    print(message)
+
+
+def fetch_exchange_rate(amount, base, to):
+
     symbols = base + ',' + to
 
     # Make the API call and get the json result
     req = requests.get(f'{BASE_URL}?access_key={CURRENCY_API_KEY}&symbols={symbols}&amount={amount}')
     req = req.json()
 
-    # Since we can't change the base currency on this API, we have to calculate it ourselves
+    # Since we can't change the base currency on this API, we have to calculate the exchange rate ourselves
     exchange_rate = req["rates"][to] / req["rates"][base]
 
-    # Calculate the resulting amount of the exchange
-    exchange_result = format(amount * exchange_rate, '.2f')
+    return exchange_rate
 
-    # Format the exchange rate to 5 decimal places
-    exchange_rate = format(exchange_rate, '.5f')
-
-    # Print out the result of the exchange
-    message = f'{amount} {base} = {exchange_result} {to} ({exchange_rate} EX)'
-    print(message)
 
 if __name__ == '__main__':
     main()
